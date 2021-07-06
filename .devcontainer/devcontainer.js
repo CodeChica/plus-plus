bd30bdda4cb406f24680ae670e4bb1d90ffb0669
mo khan <mo@mokhan.ca>
2021-07-06 15:34:54 -0600

    chore: configure the codespace

---
+++ .devcontainer/devcontainer.js
@@ -0,0 +1,15 @@
+{
+  "name": "CodeChica/plus-plus",
+  "onCreateCommand": "script/setup",
+  "forwardPorts": [
+    3000
+  ],
+  "appPort": [
+    3000
+  ],
+  "portsAttributes": {
+    "3000": {
+      "label": "http"
+    }
+  }
+}
